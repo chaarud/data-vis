618901a21a8ea027a636f1641b742b5ab0f4812c
Chaaru Dingankar <chaaru.dingankar@gmail.com>
2020-08-01 17:08:15 -0700

    Update bar.js

--- js/bar.js
+++ js/bar.js
@@ -67,10 +67,6 @@ function drawChart(picked, raw) {
   
   let x = d3.scaleBand().domain([1949, 2019]).range([0, width]);
   let y = d3.scaleLinear().domain([0, 100]).range([height, 0]);
-
-  console.log(x(1992));
-  
-  console.log(y(33)); 
   
   svg.append("g").attr("transform", "translate(0," + height + ")").call(d3.axisBottom(x));
   svg.append("g").call(d3.axisLeft(y));
@@ -80,9 +76,9 @@ function drawChart(picked, raw) {
     .data(data)
     .enter()
     .append("rect")
-      .attr("x", function(d, i) { return 400; })
-      .attr("y", function(d, i) { return 200; })
-      .attr("height", function(d, i) { return height - 300; })
+      .attr("x", function(d, i) { return x(d[0]); })
+      .attr("y", function(d, i) { return y(d[1]); })
+      .attr("height", function(d, i) { return height - y(d[1]); })
       .attr("width", x.bandwidth())
       .attr("fill", "red")
 }

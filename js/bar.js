d9ad3900fb221250d1871bdff89c44dbc47a10de
Chaaru Dingankar <chaaru.dingankar@gmail.com>
2020-08-01 17:57:43 -0700

    Update bar.js

--- js/bar.js
+++ js/bar.js
@@ -73,7 +73,7 @@ function drawChart(picked, raw) {
     .attr("transform", "translate(" + margin.left + ", " + margin.top + ")")
   
   let x = d3.scaleBand().domain(years).range([0, width]).padding(0.2);
-  let ymax = d3.max(data, function(d) { return d[1]; });
+  let ymax = d3.max(data, function(d) { return parseFloat(d[1]); });
   let y = d3.scaleLinear().domain([0, ymax]).range([height, 0]);
   
   svg.append("g").attr("transform", "translate(0," + height + ")").call(d3.axisBottom(x));
